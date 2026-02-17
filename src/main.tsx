9e11fc6559de2734644e771a5d4faa865220b7bc
[dyad] <bucolicanews@gmail.com>
2026-02-17 16:47:46 -0300

    [dyad] Adding Buffer polyfill for PIX compatibility - wrote 4 file(s), added buffer package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Buffer } from 'buffer';
+
+// Providencia o polyfill do Buffer para compatibilidade com bibliotecas Node.js no navegador
+window.Buffer = Buffer;
 
 createRoot(document.getElementById("root")!).render(<App />);
